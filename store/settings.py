0404ebeac467480af41ddd78b9e71e3351ebf926
MrZenovaDev <mrzenovadev@gmail.com>
2026-06-02 06:49:49 +0545

    some changes

--- store/settings.py
+++ store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8&sjtj^u#r4fg08nox&dj=r7h9ghta9!dqkq235y8&8omwy3)k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecommerce-django-website-first-project.onrender.com']
+ALLOWED_HOSTS = ['ecommerce-django-website-first-project.onrender']
 
 
 # Application definition
